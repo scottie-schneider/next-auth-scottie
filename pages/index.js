d5c423be983677592576f24cc04b2e4388e9ccb8
scottie-schneider <scott@laclave.co>
2020-01-18 15:10:46 -0500

    added .env file with Firebase deets, installed dotenv and created minimal next.config.js to pass env variables

--- pages/index.js
+++ pages/index.js
@@ -17,26 +17,7 @@ const Home = () => (
         To get started, edit <code>pages/index.js</code> and save to reload.
       </p>
 
-      <div className='row'>
-        <Link href='https://github.com/zeit/next.js#setup'>
-          <a className='card'>
-            <h3>Getting Started &rarr;</h3>
-            <p>Learn more about Next.js on GitHub and in their examples.</p>
-          </a>
-        </Link>
-        <Link href='https://github.com/zeit/next.js/tree/master/examples'>
-          <a className='card'>
-            <h3>Examples &rarr;</h3>
-            <p>Find other example boilerplates on the Next.js GitHub.</p>
-          </a>
-        </Link>
-        <Link href='https://github.com/zeit/next.js'>
-          <a className='card'>
-            <h3>Create Next App &rarr;</h3>
-            <p>Was this tool helpful? Let us know how we can improve it!</p>
-          </a>
-        </Link>
-      </div>
+      {process.env.FIREBASE_API_KEY}
     </div>
 
     <style jsx>{`
